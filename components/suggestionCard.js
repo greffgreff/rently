268588eeed7affeab0effc03906d7d7da44d169d
BuildTools <unconfigured@null.spigotmc.org>
2022-02-23 17:19:37 +0100

    added marquee to index

---
+++ components/suggestionCard.js
@@ -0,0 +1,7 @@
+import Styling from '../styles/suggestionCard.module.css'
+
+export default function SuggestionCard() {
+  return (
+    <div className={Styling.container}></div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import NavigationBar from '../components/navigationBar'
+import SuggestionCard from '../components/suggestionCard'
 import Styling from '../styles/index.module.css'
 
 export default function Index() {
@@ -12,7 +13,37 @@ export default function Index() {
 
       <div>
         <NavigationBar />
-        <main className={Styling.index}></main>
+        <main className={Styling.index}>
+          <div className={Styling.explore}>
+            <div className={Styling.search} />
+            <div className={Styling.suggestions}>
+              <div className={`${Styling.marquee}`}>
+                <SuggestionCard />
+                <SuggestionCard />
+                <SuggestionCard />
+                <SuggestionCard />
+                <SuggestionCard />
+                <SuggestionCard />
+              </div>
+              <div className={Styling.marquee}>
+                <SuggestionCard />
+                <SuggestionCard />
+                <SuggestionCard />
+                <SuggestionCard />
+                <SuggestionCard />
+                <SuggestionCard />
+              </div>
+              <div className={Styling.marquee}>
+                <SuggestionCard />
+                <SuggestionCard />
+                <SuggestionCard />
+                <SuggestionCard />
+                <SuggestionCard />
+                <SuggestionCard />
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
     </>
   )
