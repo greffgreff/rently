41ad78dbfa7ff7976294436575e8b33308bcc1dc
BuildTools <unconfigured@null.spigotmc.org>
2022-02-25 14:45:13 +0100

    added feature cards

---
+++ components/featureCard.js
@@ -0,0 +1,12 @@
+import Styling from '../styles/featureCard.module.css';
+
+export default function FeatureCard({ title, desc }) {
+  return (
+    <div className={Styling.container}>
+      <div className={Styling.innerContainer}>
+        <h1>{title}</h1>
+        <h3 className={Styling.desc}>{desc}</h3>
+      </div>
+    </div>
+  )
+}
