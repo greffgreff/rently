86ba40c019c93813a7fe1ebdd3bdc0f6dd9d78ab
BuildTools <unconfigured@null.spigotmc.org>
2022-02-28 00:46:47 +0100

    added spoiler comp

---
+++ components/spoiler.js
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import Styling from '../styles/spoiler.module.css';
+
+export default function Spoiler() {
+  const suggestions = ['rx 6800', 'rx 6800 xt', 'rtx 3080', 'rtx 3090', 'rx 6900 xt'];
+
+  return (
+    <div className={Styling.spoiler}>
+      <input type="checkbox" id="spoiler_" className={Styling.spoilerInput} />
+      <label for="spoiler_">
+        <i className={`fas fa-angle-right ${Styling.arrow}`} />
+      </label>
+
+      <div className={Styling.spoilerContent}>
+        {suggestions.map((s) => (
+          <div className={Styling.suggestionLink}>
+            <Link href={`/adverts?search=${s}`}>{s}</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
