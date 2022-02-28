50f6ab26a8308bab99e912b0f51937089e2df139
BuildTools <unconfigured@null.spigotmc.org>
2022-02-28 01:18:11 +0100

    added lease and login page

---
+++ pages/lease.js
@@ -0,0 +1,16 @@
+// import Styling from '../styles/lease.module.css';
+import Head from 'next/head';
+
+export default function Lease() {
+  return (
+    <>
+      <Head>
+        <title>Rently.io - Login</title>
+        <link href="https://use.fontawesome.com/releases/v5.0.1/css/all.css" rel="stylesheet" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+
+      <main>Lease</main>
+    </>
+  );
+}
---
+++ pages/login.js
@@ -0,0 +1,16 @@
+import Styling from '../styles/login.module.css';
+import Head from 'next/head';
+
+export default function Login() {
+  return (
+    <>
+      <Head>
+        <title>Rently.io - Login</title>
+        <link href="https://use.fontawesome.com/releases/v5.0.1/css/all.css" rel="stylesheet" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+
+      <main>Login</main>
+    </>
+  );
+}
