6cc036cb7b9e6b87849895bf9594334a3a26aa6a
BuildTools <unconfigured@null.spigotmc.org>
2022-02-28 01:47:28 +0100

    worked on meta info

--- components/meta.js
+++ components/meta.js
@@ -1,10 +1,18 @@
 import Styling from '../styles/meta.module.css';
+import React from 'react';
 
 export default function Meta() {
+  const months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+  const current = new Date();
+  const date = `${months[current.getMonth()]} ${current.getDate()}`;
+
   return (
     <div className={Styling.container}>
-        <div>Sarreguemines</div>
-        <div>February 27</div>
+      <div className={Styling.left}>
+        <div className={Styling.locale}><i class="fa fa-location-arrow" />Sarreguemines</div>
+        <div>{date}</div>
+      </div>
+      <div><b>EN</b></div>
     </div>
   )
 }
