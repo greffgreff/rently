7d0d10d9bd405101d6bd9f25029d4c24897f5271
chandle greff <unconfigured@null.spigotmc.org>
2022-03-20 13:30:41 +0100

    fixed btn links

--- src/components/controls/Button.tsx
+++ src/components/controls/Button.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link'
 
 export default function Button({text, icon, route, width } : { text : string, icon? : string, route? : string, width?: string }) {
   return (
-    <Link href={route ?? '/'}>
+    <a href={route}>
       <button type="button" className={`${Styling.btn} ${Styling.primary}`} style={{ width: width ?? 'auto' }}>
         <i className={`${icon} ${Styling.icon}`} style={{ display: !icon ? 'none' : '' }} />
         {text}
       </button>
-    </Link>
+    </a>
   )
 }
