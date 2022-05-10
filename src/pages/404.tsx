afa770670f1c580adf9125f1f8b0bd84ef5a1e31
chandle greff <unconfigured@null.spigotmc.org>
2022-05-10 16:06:42 +0200

    Added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,33 @@
+import Styling from './styles/error.module.css'
+import Head from 'next/head'
+import { Meta, NavigationBar } from '../components'
+import { useRouter } from 'next/router'
+
+export default function NotFound() {
+  const router = useRouter()
+  const { msg } = router.query
+  const { code } = router.query
+
+  return (
+    <>
+      <Head>
+        <title>Sorry for the trouble...</title>
+      </Head>
+
+      <main>
+        <Meta />
+        <NavigationBar />
+
+        <div className={Styling.container}>
+          <h1 className={Styling.code}>{code === 'undefined' || !code ? 404 : code}</h1>
+          <h1 className={Styling.msg}>{msg === 'undefined' || !msg ? 'Not found' : msg}</h1>
+        </div>
+      </main>
+    </>
+  )
+}
+
+NotFound.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  return { statusCode }
+}
