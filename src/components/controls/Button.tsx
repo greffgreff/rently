c829c11c545eb46e0a1eabdd52a96f575c7cd1ae
chandle greff <unconfigured@null.spigotmc.org>
2022-05-13 18:12:04 +0200

    Added on click event to comp

--- src/components/controls/Button.tsx
+++ src/components/controls/Button.tsx
@@ -1,9 +1,16 @@
+import { HTMLAttributes, MouseEvent } from 'react'
 import Styling from './styles/button.module.css'
 
-export default function Button({text, icon, route, width, submit } : { text : string, icon? : string, route? : string, width?: string, submit? : boolean }) {
+export default function Button({ text, icon, route, width, submit, onClick }: { text: string; icon?: string; route?: string; width?: string; submit?: boolean; onClick?: any }) {
+  const handleOnClick = (event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
+    if (onClick) {
+      onClick(event)
+    }
+  }
+
   return (
     <a href={route}>
-      <button type={submit ? "submit" : "button"} className={`${Styling.btn} ${Styling.primary}`} style={{ width: width ?? 'auto' }}>
+      <button type={submit ? 'submit' : 'button'} onClick={(event) => handleOnClick(event)} className={`${Styling.btn} ${Styling.primary}`} style={{ width: width ?? 'auto' }}>
         <i className={`${icon} ${Styling.icon}`} style={{ display: !icon ? 'none' : '' }} />
         {text}
       </button>
