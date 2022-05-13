d0107d198454512d28922c7fd54abb19ac92bb56
chandle greff <unconfigured@null.spigotmc.org>
2022-05-13 18:54:10 +0200

    Made count param optional

--- src/api/searchService.ts
+++ src/api/searchService.ts
@@ -13,7 +13,7 @@ export async function getListingById(id: string): Promise<Listing> {
   return res.data.results
 }
 
-export async function getRandomListings(count: number): Promise<Listing[]> {
+export async function getRandomListings(count?: number): Promise<Listing[]> {
   console.log(process.env.NEXT_PUBLIC_SEARCH_SERVICE_BASE_URL + 'api/v1/listings/random' + (count ? '?count=' + count : null))
   const res = await axios.get(process.env.NEXT_PUBLIC_SEARCH_SERVICE_BASE_URL + 'api/v1/listings/random' + (count ? '?count=' + count : null))
   return res.data.results
