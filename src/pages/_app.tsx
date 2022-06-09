7b1a90a7b89768bbacd0a2f8dcbf9c49b28d092b
chandle greff <unconfigured@null.spigotmc.org>
2022-06-09 12:27:36 +0200

    Added hotjar for UX testing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import './styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
+import { useEffect } from 'react'
+import { hotjar } from 'react-hotjar'
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  
+  useEffect(() => {
+    hotjar.initialize(3012126, 6)
+  }, [])
+  
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
