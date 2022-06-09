3c3cb6a8bea77a787f46f19c867046e7a4e5c652
chandle greff <unconfigured@null.spigotmc.org>
2022-06-09 12:59:26 +0200

    Added uuid to identify sessions for hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import './styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import { useEffect } from 'react'
 import { hotjar } from 'react-hotjar'
+import { v4 as uuid } from 'uuid'
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   
   useEffect(() => {
     hotjar.initialize(3012126, 6)
+    hotjar.identify('USER_ID', { userProperty: uuid() });
   }, [])
   
   return (
